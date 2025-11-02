fe5f43ffa4b3e4fda14f0e66707bb6c78760b5fb
PTNhatHa <ptnhatha.it@gmail.com>
2025-11-02 14:48:09 +0700

    ChatPage: update reponsive

---
+++ src/components/SnackBar.tsx
@@ -0,0 +1,64 @@
+import { LuPanelLeftClose, LuPanelLeftOpen, LuSquarePen, LuLogOut, LuUser } from "react-icons/lu";
+import HistoryItem from "../components/HistoryItem";
+import { useNavigate } from "react-router-dom";
+import { paths } from "../routes/paths";
+
+interface SidebarProps {
+    isOpenMenu: boolean;
+    setIsOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+    setListContent: React.Dispatch<React.SetStateAction<void>>;
+};
+
+const SideBar = ({ isOpenMenu, setIsOpenMenu, setListContent }: SidebarProps) => {
+    const navigate = useNavigate()
+    const handleGotoLogin = () => {
+        navigate(paths.login)
+    }
+    return (
+        <div
+            className={`
+                h-screen py-4 px-2 flex-col justify-between z-2
+                ${isOpenMenu
+                    ? 'w-[260px] flex md:relative fixed bg-background-white'
+                    : 'w-fit md:flex hidden'}
+            `}
+        >
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-row justify-between items-center">
+                    <p className={`px-2 text-lg font-bold ${isOpenMenu ? 'block' : 'hidden'}`}>MediBot</p>
+                    <div className={`p-2 hover:bg-stroke-grey rounded-lg cursor-pointer ${isOpenMenu ? 'block' : 'hidden'}`} onClick={() => setIsOpenMenu(false)}>
+                        <LuPanelLeftClose size={18} />
+                    </div>
+                    <div className={`p-2 hover:bg-stroke-grey rounded-lg cursor-pointer ${isOpenMenu ? 'hidden' : 'block'}`} onClick={() => setIsOpenMenu(true)}>
+                        <LuPanelLeftOpen size={18} />
+                    </div>
+                </div>
+                <div className="p-2 flex flex-row items-center gap-2 hover:bg-stroke-grey rounded-lg cursor-pointer" onClick={() => setListContent()}>
+                    <LuSquarePen size={18} />
+                    <p className={`text-sm ${isOpenMenu ? 'block' : 'hidden'}`}>New chat</p>
+                </div>
+                <div className={`flex-col h-full ${isOpenMenu ? 'flex' : 'hidden'}`}>
+                    <p className="px-2 pb-2 text-primary-grey">Histories</p>
+                    <HistoryItem text="Đoạn chat số 1aaaaaaaaaaaaaaaaaaaaaaaa" />
+                    <HistoryItem text="Đoạn chat số 2" />
+                    <HistoryItem text="Đoạn chat số 3" />
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <span className="w-full h-px bg-stroke-grey" />
+                <div className="flex flex-row justify-between items-center">
+                    <div className={`flex flex-row items-center gap-2 ${isOpenMenu ? 'block' : 'hidden'}`}>
+                        <span className="p-2 bg-stroke-grey rounded-full">
+                            <LuUser />
+                        </span>
+                        <p className="text-sm">Guest</p>
+                    </div>
+                    <div className="p-2 hover:bg-stroke-grey rounded-lg cursor-pointer" onClick={() => handleGotoLogin()}>
+                        <LuLogOut size={18} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default SideBar
--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,130 +1,75 @@
-import { LuPanelLeftClose, LuPanelLeftOpen, LuSquarePen, LuLogOut, LuSendHorizontal } from "react-icons/lu";
-import HistoryItem from "../components/HistoryItem";
-import { useNavigate } from "react-router-dom";
-import { paths } from "../routes/paths";
+import { LuSquarePen, LuSendHorizontal, LuMenu } from "react-icons/lu";
 import QuestionItem from "../components/QuestionItem";
 import AnswerItem from "../components/AnswerItem";
+import { useState } from "react";
+import SideBar from "../components/SnackBar";
+
+interface Content {
+    id: number,
+    ask: string,
+    answer: string
+}
 const ChatPage = () => {
-    const navigate = useNavigate()
-    const handleGotoLogin = () => {
-        navigate(paths.login)
+    const [isOpenMenu, setIsOpenMenu] = useState(true);
+    const [listContent, setListContent] = useState<Content[]>([]);
+    const handleSend = () => {
+        const newItem = {
+            id: listContent.length + 1,
+            ask: "Chào bác sĩ ảo, dạo gần đây tôi hay bị ho khan, có thể là bệnh gì vậy?",
+            answer: `Chào bạn 👋
+                            Ho khan có thể do nhiều nguyên nhân như:
+
+                            Viêm họng, cảm lạnh hoặc dị ứng thời tiết
+
+                            Viêm phế quản nhẹ
+
+                            Hút thuốc lá hoặc hít phải khói bụi
+                            Tuy nhiên, nếu ho kéo dài trên 2 tuần, bạn nên đến cơ sở y tế để kiểm tra phổi nhé 🩺`
+        }
+        setListContent([...listContent, newItem])
     }
     return (
-        <div className="w-screen h-screen flex flex-row">
-            <div className="md:w-[260px] w-fit h-screen bg-background-white py-4 px-2 flex flex-col justify-between">
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-row justify-between items-center">
-                        <p className="px-2 text-lg font-bold md:block hidden">MediBot</p>
-                        <div className="p-2 hover:bg-stroke-grey rounded-lg cursor-pointer">
-                            <LuPanelLeftClose size={18} />
-                        </div>
-                        {/* <LuPanelLeftOpen /> */}
+        <div className="w-screen h-screen flex flex-row bg-linear-to-b from-primary-white to-primary-purple">
+            <SideBar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} setListContent={()=>setListContent([])}/>
+            {isOpenMenu &&
+                <span className="fixed inset-0 bg-primary-white opacity-50 z-1 md:hidden" onClick={() => setIsOpenMenu(false)} />
+            }
+            <div className="w-full h-screen flex flex-col items-center justify-end ">
+                <div className="w-full p-4 text-lg font-bold text-primary-black md:hidden flex flex-row justify-between items-center">
+                    <div className="w-fit p-2 hover:bg-stroke-grey rounded-lg cursor-pointer" onClick={() => setIsOpenMenu(true)}>
+                        <LuMenu size={18} />
                     </div>
-                    <div className="p-2 flex flex-row items-center gap-2 hover:bg-stroke-grey rounded-lg cursor-pointer">
+                    <div className="w-fit p-2 hover:bg-stroke-grey rounded-lg cursor-pointer" onClick={() => setListContent([])}>
                         <LuSquarePen size={18} />
-                        <p className="text-sm md:block hidden">New chat</p>
-                    </div>
-                    <div className="md:flex hidden flex-col h-full">
-                        <p className="px-2 pb-2 text-primary-grey">Histories</p>
-                        <HistoryItem text="Đoạn chat số 1aaaaaaaaaaaaaaaaaaaaaaaa" />
-                        <HistoryItem text="Đoạn chat số 2" />
-                        <HistoryItem text="Đoạn chat số 3" />
                     </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                    <span className="w-full h-px bg-stroke-grey" />
-                    <div className="flex flex-row justify-between items-center">
-                        <p className="px-2 text-sm md:block hidden">Your name</p>
-                        <div className="p-2 hover:bg-stroke-grey rounded-lg cursor-pointer" onClick={() => handleGotoLogin()}>
-                            <LuLogOut size={18} />
-                        </div>
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                    <div className={`w-full ${listContent.length > 0 ? 'h-full' : 'h-fit'} overflow-y-scroll flex justify-center pl-2`}>
+                        {listContent.length == 0 ?
+                            <div className="text-center text-lg">
+                                <p>Chào bạn mình là <span className="font-bold">MediBot</span></p>
+                                <p>Hãy nói với mình triệu chứng hoặc bệnh bạn muốn tìm hiểu nhé!</p>
+                            </div>
+                            :
+                            <div className="flex flex-col gap-6 lg:w-[720px] w-full p-4">
+                                {listContent.map(item =>
+                                    <>
+                                        <QuestionItem question={item.ask} />
+                                        <AnswerItem answer={item.answer} />
+                                    </>
+                                )}
+                            </div>
+                        }
                     </div>
-                </div>
-            </div>
-            <div className="w-full h-screen flex flex-col items-center justify-end bg-gradient-to-b from-primary-white to-primary-purple">
-                <p className="w-full p-4 text-lg font-bold text-primary-black ">Tiêu đề đoạn chat</p>
-                <span className="w-full h-px bg-stroke-grey" />
-                <div className="w-full h-full overflow-y-scroll flex justify-center pl-2">
-                    <div className="flex flex-col gap-6 lg:w-[720px] w-full">
-                        <QuestionItem question="Chào bác sĩ ảo, dạo gần đây tôi hay bị ho khan, có thể là bệnh gì vậy?" />
-                        <AnswerItem answer="
-                     Chào bạn 👋
-                            Ho khan có thể do nhiều nguyên nhân như:
-
-                            Viêm họng, cảm lạnh hoặc dị ứng thời tiết
-
-                            Viêm phế quản nhẹ
-
-                            Hút thuốc lá hoặc hít phải khói bụi
-                            Tuy nhiên, nếu ho kéo dài trên 2 tuần, bạn nên đến cơ sở y tế để kiểm tra phổi nhé 🩺
-                    "/>
-                        <QuestionItem question="Tôi còn bị sốt nhẹ và đau đầu, có thể là cảm cúm không?" />
-                        <AnswerItem
-                            answer="Vâng, các triệu chứng bạn mô tả (ho, sốt nhẹ, đau đầu) rất giống cảm cúm thông thường.
-                        Bạn nên:
-
-                        Nghỉ ngơi, uống đủ nước
-
-                        Uống thuốc hạ sốt nếu nhiệt độ > 38°C
-
-                        Nếu sốt cao hơn 39°C hoặc khó thở → nên đi khám sớm."
-                        />
-                        <QuestionItem question="Chào bác sĩ ảo, dạo gần đây tôi hay bị ho khan, có thể là bệnh gì vậy?" />
-                        <AnswerItem answer="
-                     Chào bạn 👋
-                            Ho khan có thể do nhiều nguyên nhân như:
-
-                            Viêm họng, cảm lạnh hoặc dị ứng thời tiết
-
-                            Viêm phế quản nhẹ
-
-                            Hút thuốc lá hoặc hít phải khói bụi
-                            Tuy nhiên, nếu ho kéo dài trên 2 tuần, bạn nên đến cơ sở y tế để kiểm tra phổi nhé 🩺
-                    "/>
-                        <QuestionItem question="Tôi còn bị sốt nhẹ và đau đầu, có thể là cảm cúm không?" />
-                        <AnswerItem
-                            answer="Vâng, các triệu chứng bạn mô tả (ho, sốt nhẹ, đau đầu) rất giống cảm cúm thông thường.
-                        Bạn nên:
-
-                        Nghỉ ngơi, uống đủ nước
-
-                        Uống thuốc hạ sốt nếu nhiệt độ > 38°C
-
-                        Nếu sốt cao hơn 39°C hoặc khó thở → nên đi khám sớm."
-                        />
-                        <QuestionItem question="Chào bác sĩ ảo, dạo gần đây tôi hay bị ho khan, có thể là bệnh gì vậy?" />
-                        <AnswerItem answer="
-                     Chào bạn 👋
-                            Ho khan có thể do nhiều nguyên nhân như:
-
-                            Viêm họng, cảm lạnh hoặc dị ứng thời tiết
-
-                            Viêm phế quản nhẹ
-
-                            Hút thuốc lá hoặc hít phải khói bụi
-                            Tuy nhiên, nếu ho kéo dài trên 2 tuần, bạn nên đến cơ sở y tế để kiểm tra phổi nhé 🩺
-                    "/>
-                        <QuestionItem question="Tôi còn bị sốt nhẹ và đau đầu, có thể là cảm cúm không?" />
-                        <AnswerItem
-                            answer="Vâng, các triệu chứng bạn mô tả (ho, sốt nhẹ, đau đầu) rất giống cảm cúm thông thường.
-                        Bạn nên:
-
-                        Nghỉ ngơi, uống đủ nước
-
-                        Uống thuốc hạ sốt nếu nhiệt độ > 38°C
-
-                        Nếu sốt cao hơn 39°C hoặc khó thở → nên đi khám sớm."
-                        />
-                    </div>
-                </div>
-                <div className="lg:w-[720px] w-full py-4">
-                    <div className="rounded-full bg-background-white opacity-70 flex flex-row items-center">
-                        <input
-                            placeholder="Ask me..."
-                            className=" text-primary-black text-sm p-4 w-full focus:outline-none"
-                        />
-                        <div className="mr-2 p-2 hover:bg-stroke-grey rounded-full cursor-pointer" onClick={() => handleGotoLogin()}>
-                            <LuSendHorizontal size={24} />
+                    <div className="lg:w-[720px] w-full p-4">
+                        <div className="rounded-full bg-background-white opacity-70 flex flex-row items-center">
+                            <input
+                                placeholder="Ask me..."
+                                className=" text-primary-black text-sm p-4 w-full focus:outline-none"
+                            />
+                            <div className="mr-2 p-2 hover:bg-stroke-grey rounded-full cursor-pointer" onClick={() => handleSend()}>
+                                <LuSendHorizontal size={24} />
+                            </div>
                         </div>
                     </div>
                 </div>
